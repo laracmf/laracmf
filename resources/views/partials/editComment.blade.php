0b67fa4cc8e1f180c44d18375337f715a857dee1
Kit-kat1 <Katynichka95@mail.ru>
2017-01-04 10:38:23 +0200

    Fix comment editing popup

---
+++ resources/views/partials/editComment.blade.php
@@ -0,0 +1,30 @@
+@if(isRole('blogger'))
+    @include('posts.delete')
+@endif
+@if(isRole('moderator') || isRole('admin'))
+    <div id="edit_comment" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">
+        <div class="modal-dialog modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title">Edit Comment</h4>
+                </div>
+                <div class="modal-body">
+                    <form id="edit_commentform" class="form-vertical" action="" method="PATCH" data-pk="0">
+                        {{ csrf_field() }}
+                        <input id="version" name="version" value="1" type="hidden">
+                        <div class="form-group">
+                            <textarea id="edit_body" name="edit_body" class="form-control comment-box"
+                                      placeholder="Type a comment..." rows="3"></textarea>
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    <button id="edit_comment_ok" data-url="" type="button" class="btn btn-primary">OK</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+@endif
--- resources/views/posts/comment.blade.php
+++ resources/views/posts/comment.blade.php
@@ -1,27 +1,44 @@
 @if((commentOwner($comment->user_id) && !$comment->approved) || $comment->approved || !config('app.moderation'))
-<div id="comment_{!! $comment->id !!}" class="well clearfix col-xs-12 animated bounceIn{!! rand(0, 1) ? 'Left': 'Right' !!}" data-pk="{!! $comment->id !!}" data-ver="{!! $comment->version !!}">
-    @if(isRole('moderator'))
-        <div class="col-md-9 col-sm-8">
-            <p><strong>{!! $comment->author !!}</strong> - {!! html_ago($comment->created_at, 'timeago_comment_'.$comment->id) !!} @if(!$comment->approved && config('app.moderation')) <i>Pending moderator approving</i> @endif</p>
-            <p id="main_comment_{!! $comment->id !!}" class="main">{!! nl2br(e($comment->body)) !!}</p>
-        </div>
-        <div class="hidden-xs">
-            <div class="col-md-3 col-sm-4">
-                <div class="pull-right">
-                    <a id="editable_comment_{!! $comment->id !!}_1" class="btn btn-info editable" href="#edit_comment" data-pk="{!! $comment->id !!}"><i class="fa fa-pencil-square-o"></i> Edit</a> <a id="deletable_comment_{!! $comment->id !!}_1" class="btn btn-danger deletable" href="{!! route('posts.comments.destroy', ['comments' => $comment->id]) !!}"><i class="fa fa-times"></i> Delete</a>
+    <div id="comment_{!! $comment->id !!}"
+         class="well clearfix col-xs-12 animated bounceIn{!! rand(0, 1) ? 'Left': 'Right' !!}"
+         data-pk="{!! $comment->id !!}" data-ver="{!! $comment->version !!}">
+        @if(isRole('moderator') || commentOwner($comment->user_id))
+            <div class="col-md-9 col-sm-8">
+                <p><strong>{!! $comment->author !!}</strong>
+                    - {!! html_ago($comment->created_at, 'timeago_comment_'.$comment->id) !!} @if(!$comment->approved && config('app.moderation'))
+                        <i>Pending moderator approving</i> @endif</p>
+                <p id="main_comment_{!! $comment->id !!}" class="main">{!! nl2br(e($comment->body)) !!}</p>
+            </div>
+            <div class="hidden-xs">
+                <div class="col-md-3 col-sm-4">
+                    <div class="pull-right">
+                        <a id="editable_comment_{!! $comment->id !!}_1" class="btn btn-info editable"
+                           href="#edit_comment" data-pk="{!! $comment->id !!}"><i class="fa fa-pencil-square-o"></i>
+                            Edit</a> <a id="deletable_comment_{!! $comment->id !!}_1" class="btn btn-danger deletable"
+                                        href="{!! route('posts.comments.destroy', ['comments' => $comment->id]) !!}"><i
+                                    class="fa fa-times"></i> Delete</a>
+                    </div>
                 </div>
             </div>
-        </div>
-        <div class="visible-xs">
-            <div class="col-xs-12">
-                <a id="editable_comment_{!! $comment->id !!}_2" class="btn btn-info editable" href="#edit_comment" data-pk="{!! $comment->id !!}"><i class="fa fa-pencil-square-o"></i> Edit</a> <a id="deletable_comment_{!! $comment->id !!}_2" class="btn btn-danger deletable" href="{!! route('posts.comments.destroy', ['comments' => $comment->id]) !!}"><i class="fa fa-times"></i> Delete</a>
+            <div class="visible-xs">
+                <div class="col-xs-12">
+                    <a id="editable_comment_{!! $comment->id !!}_2" class="btn btn-info editable" href="#edit_comment"
+                       data-pk="{!! $comment->id !!}"><i class="fa fa-pencil-square-o"></i> Edit</a> <a
+                            id="deletable_comment_{!! $comment->id !!}_2" class="btn btn-danger deletable"
+                            href="{!! route('posts.comments.destroy', ['comments' => $comment->id]) !!}"><i
+                                class="fa fa-times"></i> Delete</a>
+                </div>
             </div>
-        </div>
-    @else
-        <div class="col-xs-12">
-            <p><strong>{!! $comment->author !!}</strong> - {!! html_ago($comment->created_at, 'timeago_comment_'.$comment->id) !!} @if(!$comment->approved && config('app.moderation')) <i>Pending moderator approving</i> @endif</p>
-            <p id="main_comment_{!! $comment->id !!}" class="main">{!! nl2br(e($comment->body)) !!}</p>
-        </div>
-    @endif
-</div>
+        @else
+            <div class="col-xs-12">
+                <p><strong>{!! $comment->author !!}</strong>
+                    - {!! html_ago($comment->created_at, 'timeago_comment_'.$comment->id) !!} @if(!$comment->approved && config('app.moderation'))
+                        <i>Pending moderator approving</i> @endif</p>
+                <p id="main_comment_{!! $comment->id !!}" class="main">{!! nl2br(e($comment->body)) !!}</p>
+            </div>
+        @endif
+    </div>
 @endif
+@section('bottom')
+    @include('partials.editComment')
+@stop
