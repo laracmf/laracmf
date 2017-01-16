9e15251f432055cd4f7ffddcc5cfae1b84fa649c
Kit-kat1 <Katynichka95@mail.ru>
2017-01-16 18:23:18 +0200

    Add php7 installation

--- ci/install.sh
+++ ci/install.sh
@@ -2,7 +2,7 @@
 
 # Update packages and install composer and PHP dependencies.
 apt-get update -yqq
-apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq
+apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq zip unzip php7.0 php7.0-fpm php7.0-mysql -y
 
 # Install Composer and project dependencies.
 curl -sS https://getcomposer.org/installer | php
