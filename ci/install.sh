683776dd7693f9f47eb491c435a07d9e279a07e0
Kit-kat1 <Katynichka95@mail.ru>
2017-01-17 09:27:50 +0200

    Fix path to .env.testing

--- ci/install.sh
+++ ci/install.sh
@@ -7,13 +7,12 @@ set -xe
 apt-get update -yqq
 apt-get install git libcurl4-gnutls-dev libicu-dev libmcrypt-dev libvpx-dev libjpeg-dev libpng-dev libxpm-dev zlib1g-dev libfreetype6-dev libxml2-dev libexpat1-dev libbz2-dev libgmp3-dev libldap2-dev unixodbc-dev libpq-dev libsqlite3-dev libaspell-dev libsnmp-dev libpcre3-dev libtidy-dev -yqq zip unzip
 
-php -v
 # Install Composer and project dependencies.
 curl -sS https://getcomposer.org/installer | php
 php composer.phar install
 
 # Copy over testing configuration.
-cp .env.testing .env
+cp config/env/.env.testing .env
 
 # Generate an application key. Re-cache.
 php artisan key:generate
