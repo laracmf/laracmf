4abcd7946ef4560745f8e50b37a8b463b14bcd58
AlexFloppy <alex.57g@gmail.com>
2017-02-27 18:56:13 +0200

    Enable valid error messages

--- resources/views/partials/notifications.blade.php
+++ resources/views/partials/notifications.blade.php
@@ -1,7 +1,13 @@
 @if (isset($errors) && count($errors->all()) > 0)
+
 <div class="alert alert-danger cms-alert">
     <a class="close" data-dismiss="alert">×</a>
-    Please check the form below for errors
+    <ul class="list-unstyled">
+        @foreach($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+    {{--Please check the form below for errors--}}
 </div>
 @endif
 
